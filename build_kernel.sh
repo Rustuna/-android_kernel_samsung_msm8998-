b0f9c9223dd03e14af45ac0bb34c277620742223
Rustuna <73030053+Rustuna@users.noreply.github.com>
2021-06-01 12:18:40 +0800

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 export ARCH=arm64
-export PATH=$(pwd)/toolchain/gcc-cfp/gcc-cfp-jopp-only/aarch64-linux-android-4.9/bin:$PATH
+export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH
 
 mkdir out
 
 make -C $(pwd) O=$(pwd)/out CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android greatqlte_chn_open_defconfig
-make -j64 -C $(pwd) O=$(pwd)/out CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android
+make -j4 -C $(pwd) O=$(pwd)/out CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android
  
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
-
